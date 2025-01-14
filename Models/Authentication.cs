20676997dbcbab97ed3b4af6a1488dba03f1bfe9
triaxyd <triantafyllosxyd@gmail.com>
2025-01-14 17:28:00 +0200

    added requirements for AuthenticationVM

--- Models/Authentication.cs
+++ Models/Authentication.cs
@@ -1,8 +1,17 @@
-﻿namespace MVCProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCProject.Models
 {
     public class Authentication
     {
+        [Required]
+        [MinLength(4)]
+        [MaxLength(20)]
         public string Username { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        [MaxLength(15)]
         public string Password { get; set; }
     }
 }
